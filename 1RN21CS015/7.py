8972e0e9f5bad9375a53de9f2b89874aeaa4b7bc
Aditya Prakash <112635806+Adityaprakash754@users.noreply.github.com>
2023-06-23 18:53:40 +0530

    Update 7.py

--- 1RN21CS015/7.py
+++ 1RN21CS015/7.py
@@ -1,7 +1,6 @@
 # pallindrome or not - Lab 2
 
 # Method 1
-'''
 n = int(input("Enter the number: "))
 
 num = n
@@ -16,8 +15,12 @@ if(num2 == n):
     print("It is a pallindrome")
 else:
     print("It is not a pallindrome")
+
+for i in range(10):
+    if str_val.count(str(i)) > 0:
+        print(str(i),"appears", str_val.count(str(i)), "times");
      
-'''
+#**************************************************************************
 
 #Method 2
 n = input("Enter the number: ")
