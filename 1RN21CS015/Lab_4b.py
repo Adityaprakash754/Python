cc2b48a9da92051c829e8bcc8a4010e11ffe5d9f
Aditya Prakash <112635806+Adityaprakash754@users.noreply.github.com>
2023-07-24 23:07:10 +0530

    Update Lab_4b.py
    Corrected the logic

--- 1RN21CS015/Lab_4b.py
+++ 1RN21CS015/Lab_4b.py
@@ -1,11 +1,17 @@
 user_dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100}
 
 def romDec(input):
+    n = len(input)
     res = 0
-    for i in input:
-        res += user_dict[i]
+    for i in range (n-1):
+        if user_dict[input[i]] < user_dict[input[i+1]]:
+            res -= user_dict[input[i]]
+        else:
+            res += user_dict[input[i]]
+
+    res += user_dict[input[i+1]]
 
     print(res)
 
 input = input("Enter the roman no ")
-romDec(input)
+romDec(input) 
