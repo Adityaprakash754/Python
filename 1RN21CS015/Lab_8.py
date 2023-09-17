bab9162704ddabdc57607d19d49b0f5ebfdc9243
Aditya Prakash <112635806+Adityaprakash754@users.noreply.github.com>
2023-09-17 16:03:25 +0530

    Update Lab_8.py

--- 1RN21CS015/Lab_8.py
+++ 1RN21CS015/Lab_8.py
@@ -1,3 +1,24 @@
+# Method - 1
+class A:
+   def Pal(self, val:str):
+       if(val!=val[::-1]):
+           print("Not palindrome")
+       else:
+           print("Is Palindrome")
+       
+class B(A):
+   def Pal(self, val:int):
+       super().Pal(str(val))
+   
+A().Pal(input("enter  String: "))
+B().Pal(int(input("enter  Number: ")))
+
+
+#**********************************************************
+
+#Method - 2 
+
+
 class PaliStr:
     def __init__(self):
         self.isPali = False
